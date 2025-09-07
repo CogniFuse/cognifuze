181c9974e9d8998f4974f99fc9073178ad3be57e
Rishabh Chaudhary <rishabhc329@gmail.com>
2025-09-07 22:34:53 +0530

    Updated deply.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,20 @@
+#!/bin/bash
+set -e  # Exit immediately if a command exits with a non-zero status
+
+# Build the project
 npm run build
+
+# Commit the build folder
 git add dist -f
 git commit -m "chore: update build $(date)"
+
+# Create a subtree branch for dist
 git subtree split --prefix dist -b build-branch
+
+# Push subtree to build branch in remote
 git push origin build-branch:build --force
+
+# Delete temporary branch
 git branch -D build-branch
+
+echo "Deployment completed!"
